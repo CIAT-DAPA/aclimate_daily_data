a48deac2418164910217171ffdddb94ef9ec8da4
santiagoC <s.calderon@cgiar.org>
2023-10-13 10:58:17 -0500

    class

---
+++ src/aclimate_daily_data/aclimate_run.py
@@ -0,0 +1,16 @@
+import argparse
+
+from dateutil.relativedelta import relativedelta
+
+from aclimate_daily_data.data_loader import DownloadDailyData
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Resampling script")
+    
+    dd = DownloadDailyData()
+    dd.main()
+
+
+if __name__ == "__main__":
+    main()
